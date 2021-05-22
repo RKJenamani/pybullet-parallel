8c3495d3862b9ac77f6f21ee7b418e133b210e19
RKJenamani <rajatkj11@gmail.com>
2021-05-22 10:08:03 +0530

    hybrid approach

---
+++ hybrid.py
@@ -0,0 +1,142 @@
+# import threading
+import multiprocessing 
+import time
+import numpy as np
+from types import SimpleNamespace
+import pybullet
+import pybullet_data
+import pybullet_utils.bullet_client as bc
+
+def setup_exp(p,offset_x,offset_y):
+    mu = 0.65
+    object_ids = []
+
+    local_path = "./"
+
+    state = ([-0.0404755 +offset_x, -0.00112935 +offset_y, 0.183326,0.000183599, 0.0274296, -3.12269])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/024_bowl/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([-0.031447 +offset_x, 0.00245332 +offset_y, 0.231471,1.11077, 0.116883, 1.88827])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/037_scissors/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([-0.0342039 +offset_x, -0.0411717 +offset_y, 0.221495,-1.21418, -0.411491, 1.3281])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/011_banana/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([-0.00109234 +offset_x, 0.00217348+offset_y, 0.0940739,0.603044, -1.5319, 2.60045])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/004_sugar_box/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([0.0431414 +offset_x, -0.0814738+offset_y, 0.100775,-1.56183, 0.0200383, -0.0332266])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/010_potted_meat_can/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([-0.0996265 +offset_x, -0.0109982+offset_y, 0.229266,1.09516, -0.0324135, 1.93206])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/037_scissors/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([0.0391189 +offset_x, -0.0793964+offset_y, 0.142291,-3.03823, -0.084093, 1.51302])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/009_gelatin_box/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([-0.039549 +offset_x, -0.000712143+offset_y, 0.141748,-0.00616443, 0.0036889, -1.53266])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/008_pudding_box/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([0.0518593 +offset_x, -0.113949+offset_y, 0.206423,-2.99895, -0.0929695, 1.27618])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/007_tuna_fish_can/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([0.0575085 +offset_x, -0.102112+offset_y, 0.171442,-3.01417, -0.111824, 1.44483])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/007_tuna_fish_can/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+    state = ([0.000732725 +offset_x, 0.000258393+offset_y, 0.0369799,-2.1136, -1.55321, 0.539161])
+    req = SimpleNamespace(o_x=state[0],o_y=state[1],o_z=state[2],o_r = state[3], o_p = state[4], o_yaw = state[5], type = 8)
+    xyz = [req.o_x, req.o_y, req.o_z]
+    rpy = [req.o_r, req.o_p, req.o_yaw]
+    body_id = p.loadURDF(local_path + "models/003_cracker_box/model.urdf", xyz, p.getQuaternionFromEuler(rpy))        
+    p.changeDynamics(body_id, -1, lateralFriction=mu)
+    object_ids.append(body_id)
+
+def run_sim(num_envs):
+
+    p = bc.BulletClient(connection_mode=pybullet.DIRECT)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.setGravity(0,0,-10)
+    plane = p.loadURDF("plane.urdf")
+    p.setRealTimeSimulation(0)
+
+    for num in range(0,num_envs):
+        setup_exp(p,num,num)
+
+    for _ in range(1000):
+        p.stepSimulation()
+
+    return None
+
+def worker(inst):
+    pos = run_sim(2)
+
+
+for iter in range(1,13,3):
+
+    num_parallel = iter
+    start_time = time.time()
+
+    w = []
+    for i in range(num_parallel):
+        wi = multiprocessing.Process(target=worker, args={"inst":i})
+        w.append(wi)
+
+    for i in range(num_parallel):
+        w[i].start()
+
+    for i in range(num_parallel):
+        w[i].join()
+
+    end_time = time.time()
+
+    print("Total time taken: {0:.2f} | Parallel Envs: {1} | Time taken per simulation: {2:.2f} ".format((end_time-start_time),num_parallel*2,(end_time-start_time)/(num_parallel*2)))
